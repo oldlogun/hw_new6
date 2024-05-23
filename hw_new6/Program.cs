58ccfdf5063c9b39cd3e0bb1e6056a2782ef77af
Vitaly <Vitaly@DESKTOP-0IE18EK>
2024-05-23 18:56:56 +0300

    Добавьте файлы проекта.

---
+++ hw_new6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_new6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько людей в очереди перед вами: ");
+            int peopleQueue = Convert.ToInt32(Console.ReadLine());
+
+            int timeStepInMin = 13;
+            int totalTimeInMin = peopleQueue * timeStepInMin;
+            int hourToMin = 60;
+            int timeInHours = totalTimeInMin / hourToMin;
+            int timeInMin = totalTimeInMin % hourToMin;
+
+            Console.WriteLine($"Время ожидания: {timeInHours} часов {timeInMin} минут.");
+        }
+    }
+}
